9edf6cf8e2ff158775d77c70ad3822d91d83a05d
MD: Mehedi Al Hasan <136561618+sawon21@users.noreply.github.com>
2024-11-23 10:26:12 +0600

    Update review.js

--- review.js
+++ review.js
@@ -1,8 +1,10 @@
         // Default Configuration
-        let CACHE_NAME = "smart_review_cache_v04"; // Default cache name
+        let CACHE_NAME = "smart_review_cache_v05"; // Default cache name
         const DEFAULT_REVIEWS = [
         
-            { name: "Abu Rayhan Rahad", gender: "male", date: "01 Nov 2024", text: "Website টা পুরাই আগুন 🔥 🔥 🔥" }
+            { name: "Abu Rayhan Rahad", gender: "male", date: "01 Nov 2024", text: "Website টা পুরাই আগুন 🔥 🔥 🔥" },
+{ name: "Emon", gender: "male", date: "03 Nov 2024", text: "Admission সময়ের সবচেয়ে গুরুত্বপূর্ণ বিষয়। ধন্যবাদ 😊" }
+
         ];
 
         // DOM Elements
